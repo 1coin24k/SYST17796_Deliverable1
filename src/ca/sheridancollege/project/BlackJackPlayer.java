4a3fdae0b75a2c3b6193483f7522c5f5cb9f7f7f
su11ya <85003453+su11ya@users.noreply.github.com>
2021-08-17 23:19:03 -0400

    Update BlackJackPlayer.java

--- src/ca/sheridancollege/project/BlackJackPlayer.java
+++ src/ca/sheridancollege/project/BlackJackPlayer.java
@@ -1,64 +1,114 @@
-/**
- * SYST 17796 Project Winter 2019 Base code.
- * Students can modify and extend to implement their game.
- * Add your name as a modifier and the date!
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package ca.sheridancollege.project;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 /**
- * The class that models your game. You should create a more specific
- * child of this class and instantiate the methods given.
- * @author dancye, 2018
+ *
+ * @author YutingLi
  */
-public abstract class Game 
-{
-    private final String gameName;//the title of the game
-    private ArrayList <Player> players;// the players of the game
-    
-    public Game(String givenName)
-    {
-        gameName = givenName;
-        players = new ArrayList();
-    }
+public class BlackJackPlayer extends Player {
+    private ArrayList<Card> cardsOnTable ;
+    private ArrayList<BlackJackCard> cardsOnHand = new ArrayList<>();
+    private int totalPoint ;
 
     /**
-     * @return the gameName
+     * A constructor that allows you to set the player's unique ID
+     *
+     * @param name the unique ID to assign to this player.
      */
-    public String getGameName() 
-    {
-        return gameName;
-    }
-    
-     /**
-     * @return the players of this game
-     */
-    public ArrayList <Player> getPlayers() 
-    {
-        return players;
+    public BlackJackPlayer(String name) {
+        super(name);
     }
 
-    /**
-     * @param players the players of this game
-     */
-    public void setPlayers(ArrayList <Player> players) 
-    {
-        this.players = players;
+    public void play(){
+        Scanner scanner = new Scanner(System.in);
+        if(cardsOnHand.size() ==0 ){
+            drawTwoCard();
+            setTotalPoint(checkPoint());
+        }
+        if("Player".equals(getPlayerID())){
+            if (cardsOnTable.size()>0 ){
+                String userSelect = "2";
+                while (checkPoint() < 21 && "2".equals(userSelect)){
+                    showCardsOnHand();
+                    System.out.print("Do you want Stand[1] or Hit[2], please choose 1 or 2 : ");
+                    userSelect = scanner.next();
+                    if ("2".equals(userSelect)){
+                        hit();
+                    }
+                }
+                setTotalPoint(checkPoint());
+            }
+        }
     }
-    
-    /**
-     * Play the game. This might be one method or many method calls depending
-     * on your game.
-     */
-    public abstract void play();
-    
-    /**
-     * When the game is over, use this method to declare and display a winning
-     * player.
-     */
-    public abstract void declareWinner();
 
-   
-    
-}//end class
+
+    private int checkPoint(){
+        List<BlackJackCard> aceCardOnHand = new ArrayList<>();
+        int totalPoint = 0;
+        for (BlackJackCard card : cardsOnHand){
+            totalPoint += card.getValue();
+            if (card.getValue() == 11){
+                aceCardOnHand.add(card);
+            }
+        }
+
+        for(BlackJackCard card : aceCardOnHand) {
+            if(totalPoint > 21) {
+                totalPoint = totalPoint - 10;
+            }
+        }
+        return totalPoint;
+    }
+
+    private void hit(){
+        if(cardsOnTable.size()>0){
+            BlackJackCard blackJackCard = (BlackJackCard) cardsOnTable.remove(0);
+            cardsOnHand.add(blackJackCard);
+        }
+    }
+
+    private void drawTwoCard(){
+        hit();
+        hit();
+    }
+
+    public ArrayList<Card> getCardsOnTable() {
+        return cardsOnTable;
+    }
+
+    public void setCardsOnTable(ArrayList<Card> cardsOnTable) {
+        this.cardsOnTable = cardsOnTable;
+    }
+
+    public int getTotalPoint() {
+        return totalPoint;
+    }
+
+    public void setTotalPoint(int totalPoint) {
+        this.totalPoint = totalPoint;
+    }
+
+    public ArrayList<BlackJackCard> getCardsOnHand() {
+        return cardsOnHand;
+    }
+
+    public void setCardsOnHand(ArrayList<BlackJackCard> cardsOnHand) {
+        this.cardsOnHand = cardsOnHand;
+    }
+
+    public void showCardsOnHand () {
+        System.out.println(getPlayerID()+" cards on hand, total point is ["+checkPoint()+"]");
+        for(BlackJackCard card : getCardsOnHand()){
+            System.out.println("["+card.getSuit()+"] "+card.getValue());
+        }
+        System.out.println("====================== ");
+    }
+}
