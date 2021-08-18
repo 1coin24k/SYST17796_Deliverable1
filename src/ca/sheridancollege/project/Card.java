3d5430a8556d6b72ed370d083d570b45d5b33bb7
su11ya <85003453+su11ya@users.noreply.github.com>
2021-08-17 22:22:25 -0400

    Update Card.java

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -5,22 +5,60 @@
  */
 package ca.sheridancollege.project;
 
+import java.util.ArrayList;
+
 /**
- * A class to be used as the base Card class for the project. Must be general
- * enough to be instantiated for any Card game. Students wishing to add to the code 
- * should remember to add themselves as a modifier.
+ * The class that models your game. You should create a more specific
+ * child of this class and instantiate the methods given.
  * @author dancye, 2018
  */
-public abstract class Card 
+public abstract class Game 
 {
-    //default modifier for child classes
+    private final String gameName;//the title of the game
+    private ArrayList <Player> players;// the players of the game
+    
+    public Game(String givenName)
+    {
+        gameName = givenName;
+        players = new ArrayList();
+    }
+
+    /**
+     * @return the gameName
+     */
+    public String getGameName() 
+    {
+        return gameName;
+    }
+    
+     /**
+     * @return the players of this game
+     */
+    public ArrayList <Player> getPlayers() 
+    {
+        return players;
+    }
+
+    /**
+     * @param players the players of this game
+     */
+    public void setPlayers(ArrayList <Player> players) 
+    {
+        this.players = players;
+    }
     
     /**
-     * Students should implement this method for their specific children classes 
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
+     * Play the game. This might be one method or many method calls depending
+     * on your game.
      */
+    public abstract void play();
     
-    @Override
-    public abstract String toString();
+    /**
+     * When the game is over, use this method to declare and display a winning
+     * player.
+     */
+    public abstract void declareWinner();
+
+   
     
-}
+}//end class
